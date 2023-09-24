1375a3517876acfb0a853c57059b92ea1de2b16c
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2023-09-25 00:20:50 +0530

    some files changed

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup,login,dashboard } from '../controller/signup-controller.js';
+import { signup,login,dashboard } from '../controller/auth_controller.js';
 import { emailcheckSignUp} from '../middleware/emailchecking.js';
 import { checklogin } from '../middleware/checklogin.js';
 
